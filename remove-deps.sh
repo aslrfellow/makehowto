ca2f70e65dc91400b60f1bc8ba204a7004b3d8cb
oketo <oketo@lasalles-MBP.hsd1.il.comcast.net>
2019-11-29 21:23:05 -0600

    simple make howto

---
+++ remove-deps.sh
@@ -0,0 +1,42 @@
+
+# delete
+
+rm -rf ./Makefile.in
+rm -rf ./aclocal.m4
+rm -rf ./autom4te.cache
+rm -rf ./compile
+rm -rf ./config.guess
+rm -rf ./config.h
+rm -rf ./config.h.in
+rm -rf ./config.log
+rm -rf ./config.status
+rm -rf ./config.sub
+rm -rf ./configure
+rm -rf ./depcomp
+rm -rf ./install-sh
+rm -rf ./libtool
+rm -rf ./ltmain.sh
+rm -rf ./m4
+rm -rf ./missing
+rm -rf ./stamp-h1
+
+rm -rf./src/.deps
+rm -rf./src/Makefile
+rm -rf./src/Makefile.in
+
+rm -rf./doc/Makefile
+rm -rf./doc/Makefile.in
+
+rm -rf./examples/Makefile
+rm -rf./examples/Makefile.in
+
+rm -rf./man/Makefile
+rm -rf./man/Makefile.in
+
+rm -rf./man/Makefile
+rm -rf./man/Makefile.in
+
+rm -rf./scripts/Makefile
+rm -rf./scripts/Makefile.in
+
+
